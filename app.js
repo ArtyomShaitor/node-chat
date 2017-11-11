7da075a01525052a30e28bc96603b12aa691f792
Atryom <artyomshaitor@gmail.com>
2017-11-12 00:05:52 +0200

    finishing prototype

--- app.js
+++ app.js
@@ -26,13 +26,14 @@ io.on('connection', function(socket){
 
     socket.on('send message', function(msg){
         messages.push(msg);
-
         socket.broadcast.emit('get message', msg);
     });
+
+    socket.emit('last 100 messages', messages.slice(0, 100));
 
     io.emit('now online', nowOnline);
 });
 
-http.listen(3000, function(){
+http.listen(8080, function(){
     console.log('listening on *:3000');
 });
